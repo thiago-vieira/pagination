da758d86ed07a9b9ae6ceb6a69bd8bdacb03fac9
Thiago Vieira <thiago.rails@gmail.com>
2018-07-24 00:47:11 -0300

    fc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paginacao::Application.config.secret_token = '2723ef3bfda3209f18ba79e42452c5ee7bf38cc3a86bced334895822b1caf7e0230b536ce15ed8eda7a189e7d8ab77d469a362c048dfeedfa9c4ef19ecbc6c29'
